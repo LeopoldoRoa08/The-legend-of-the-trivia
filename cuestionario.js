0fb6c2e5fc94e02a894e509d4b14c81e7767f79d
Leopoldo Roa <166873477+LeopoldoRoa08@users.noreply.github.com>
2025-06-03 22:21:17 -0400

    Revert "a"
    This reverts commit 212e2d731867939809da2a39668c65b058c25aa9.

--- cuestionario.js
+++ cuestionario.js
@@ -132,7 +132,7 @@ function siguientePregunta() {
         localStorage.setItem("ranking", JSON.stringify(ranking));
 
         // Redirigir a la página de resultados
-        window.location.href = 'Cuestionariofinalizado.html';
+        window.location.href = 'cuestionariofinalizado.html';
         return;
     }
 
